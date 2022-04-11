714f624bc84bc8f1c07dd7cd3986a72fea51d686
Andrey Gorshkov <ukr.web.ua@gmail.com>
2022-04-11 12:41:49 +0300

    2 request

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -1,7 +1,27 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
+
 public class Test2 extends WebDriverClass{
 
-    public void Class3(){
-        System.out.println("test2");
+
+    private String siteUrl = "https://qa2.eatstreet.com";
+
+    @org.testng.annotations.Test
+    public void openRegister(){
+        driver.get(siteUrl + "/create-account");
+
+        String genName = System.currentTimeMillis() + "_andrey@gmail.com";
+        String passTest = "Zaqxsw123qaz";
+
+        setTextByLocator("//input[@id='email']",genName);
+        setTextByLocator("//input[@id='password']",passTest);
+        setTextByLocator("//input[@id='passwordAgain']",passTest);
+
+        clickByLocator("//button[@id='signup']");
+
+        waitElementByLocator("//a[@id='menu-my-account']", 10);
+
+        Assert.assertNotNull(driver.findElement(By.xpath("//a[@id='menu-my-account']")));
     }
 
 }
